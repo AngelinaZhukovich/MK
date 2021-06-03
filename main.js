ffa5472e7275bf0973fb8cdf5e4ab8192992dc4b
AngelinaZhukovich <angelinka04@mail.ru>
2021-06-03 21:53:22 +0300

    homework2

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-alert('Hello');
-console.log('Hello')
+const player1 = {
+  name: 'Pit',
+  hp: 100,
+  img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+  weapon: ['Nozh', 'Mech'],
+  attack: function () {
+    console.log(name + "Fight...")
+  }
+}
+
+const player2 = {
+  name: 'Jo',
+  hp: 100,
+  img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+  weapon: ['Nozh', 'Mech'],
+  attack: function () {
+    console.log(name + "Fight...")
+  }
+}
+
